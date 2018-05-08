e17b1929ae712fa5699090fbe13d31a119c59ba6
Lysakov Ivan <ivanlysakov@gmail.com>
2018-05-08 13:26:21 +0300

    project creation

---
+++ parking/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! This is BeeParking");
+            Console.ReadKey();
+        }
+    }
+}
